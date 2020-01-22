c3379fe4b387cc6cb189903c5ded7914587f209f
twinkle <twinkle18@navgurukul.org>
2020-01-22 15:57:06 +0530

    made cards with caption

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+import Card from './card';
+function App() {
+  return (
+    <div className="App">
+      <Card/>
+    </div>
+  );
+}
+
+export default App;
